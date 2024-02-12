2ac0a3160b4e5834cd35d1fb62ca0dcf583085c4
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2024-02-12 15:02:43 -0500

    fixed structure of pygame project

--- FrontEnd/test.py
+++ FrontEnd/test.py
@@ -1,96 +1,127 @@
 import pygame
 import math
 
-# Setup
-pygame.init()
-screen = pygame.display.set_mode((750, 500))
-clock = pygame.time.Clock()  # Used to calculate Delta time
-running = True
-dt = 0  # Stores seconds between frames
 
-# Important positions
-PLAYER1 = (50, 200, 15, 100)
-PLAYER2 = (685, 200, 15, 100)
-BALL = (355, 230, 40, 40)
+def restart(player: int) -> None:
+    """
+    Parameters:
+    player: an int representing the index of the player whose score should be incremented
 
-# Values that can change [[x, y], [width, height]]
-curr_player1 = [[50, 200], [15, 100]]
-curr_player2 = [[685, 200], [15, 100]]
-curr_ball = [[355, 230], [40, 40]]
+    Resets the game board
+    """
+    global dx, dy, angle, SPEED, num_collisions, scores, BALL
 
-# Movement Values
-dx = 125
-dy = 0
-angle = 110
-first_collision = True
 
-while running:
+    # Update variables
+    # scores[player] += 1
+    # num_collisions = 0
+    angle = 120 if sum(scores) % 2 == 0 else 300
+    # SPEED *= 2
+    dx = 25.25 if sum(scores) % 2 == 0 else -25.25
+    dy = 0
 
-    # Limits FPS 60
-    # Calculates delta time for framerate-independent physics
-    dt = clock.tick(60) / 1000
+    # Update canvas
+    canvas.coords(ball, (355, 230, 395, 270))
+    canvas.itemconfig(scoreboard, text=f"{scores[0]} - {scores[1]}")
 
-    # Check events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    # Cover last frame's graphics
-    screen.fill("white")
+def main():
 
-    # Move the ball
-    curr_ball[0][0] += dx * dt
-    curr_ball[0][1] += dy * dt
+    # Setup
+    pygame.init()
+    screen = pygame.display.set_mode((750, 500))
+    clock = pygame.time.Clock()  # Used to calculate Delta time
+    running = True
+    dt = 0  # Stores seconds between frames
 
-    ball = pygame.draw.ellipse(screen, 'pink', (curr_ball[0][0], curr_ball[0][1], curr_ball[1][0], curr_ball[1][1]))
-    player1 = pygame.draw.rect(screen, 'red', (curr_player1[0][0], curr_player1[0][1], curr_player1[1][0], curr_player1[1][1]))
-    player2 = pygame.draw.rect(screen, 'blue', (curr_player2[0][0], curr_player2[0][1], curr_player2[1][0], curr_player2[1][1]))
+    # Important positions
+    PLAYER1 = (50, 200, 15, 100)
+    PLAYER2 = (685, 200, 15, 100)
+    BALL = (355, 230, 40, 40)
 
-    # Check collisions
-    if player1.colliderect(ball) or player2.colliderect(ball):
-        if first_collision:
-            dy = 125*math.sin(math.radians(angle))
-            first_collision = False
-        dx*=-1.1
+    # Values that can change [[x, y], [width, height]]
+    curr_player1 = [[50, 200], [15, 100]]
+    curr_player2 = [[685, 200], [15, 100]]
+    curr_ball = [[355, 230], [40, 40]]
+
+    # Movement Values
+    dx = 125
+    dy = 0
+    angle = 110
+    first_collision = True
+
+    while running:
+
+        # Limits FPS 60
+        # Calculates delta time for framerate-independent physics
+        dt = clock.tick(60) / 1000
+
+        # Check events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Cover last frame's graphics
+        screen.fill("white")
+
+        # Move the ball
         curr_ball[0][0] += dx * dt
         curr_ball[0][1] += dy * dt
 
-        print(dy)
+        ball = pygame.draw.ellipse(screen, 'pink', (curr_ball[0][0], curr_ball[0][1], curr_ball[1][0], curr_ball[1][1]))
+        player1 = pygame.draw.rect(screen, 'red', (curr_player1[0][0], curr_player1[0][1], curr_player1[1][0], curr_player1[1][1]))
+        player2 = pygame.draw.rect(screen, 'blue', (curr_player2[0][0], curr_player2[0][1], curr_player2[1][0], curr_player2[1][1]))
 
-    if curr_ball[0][1] <=0 or curr_ball[0][1] >= 470:
-        dy*=-1
+        # Check collisions
+        if player1.colliderect(ball) or player2.colliderect(ball):
+            if first_collision:
+                dy = 125*math.sin(math.radians(angle))
+                first_collision = False
+            dx*=-1.1
+            curr_ball[0][0] += dx * dt
+            curr_ball[0][1] += dy * dt
 
-    # Handle player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        delta = 300 * dt
+            print(dy)
 
-        # If this movement would not take the player out of bounds
-        if curr_player1[0][1] - delta >= 0:
-            curr_player1[0][1] -= delta
+        if curr_ball[0][1] <=0 or curr_ball[0][1] >= 470:
+            dy*=-1
 
-    if keys[pygame.K_s]:
-        delta = 300 * dt
+        # Handle player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            delta = 300 * dt
 
-        # If this movement would not take the player out of bounds
-        if curr_player1[0][1] + delta <= 400:
-            curr_player1[0][1] += delta
+            # If this movement would not take the player out of bounds
+            if curr_player1[0][1] - delta >= 0:
+                curr_player1[0][1] -= delta
 
-    if keys[pygame.K_UP]:
-        delta = 300 * dt
+        if keys[pygame.K_s]:
+            delta = 300 * dt
 
-        # If this movement would not take the player out of bounds
-        if curr_player2[0][1] - delta >= 0:
-            curr_player2[0][1] -= delta
+            # If this movement would not take the player out of bounds
+            if curr_player1[0][1] + delta <= 400:
+                curr_player1[0][1] += delta
 
-    if keys[pygame.K_DOWN]:
-        delta = 300 * dt
+        if keys[pygame.K_UP]:
+            delta = 300 * dt
 
-        # If this movement would not take the player out of bounds
-        if curr_player2[0][1] + delta <= 400:
-            curr_player2[0][1] += delta            
+            # If this movement would not take the player out of bounds
+            if curr_player2[0][1] - delta >= 0:
+                curr_player2[0][1] -= delta
 
-    # Update display
-    pygame.display.flip()
+        if keys[pygame.K_DOWN]:
+            delta = 300 * dt
 
-pygame.quit()
+            # If this movement would not take the player out of bounds
+            if curr_player2[0][1] + delta <= 400:
+                curr_player2[0][1] += delta            
+
+        # Update display
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()
